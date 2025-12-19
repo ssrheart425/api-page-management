ffbb773cb91d4957d2bfef6b963bb5925ebb1d2e
heart <ssrheart1337@gmail.com>
2025-12-19 16:23:00 +0800

    fix: base mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,12 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/admin/' : '/', // ⚠️ 关键：生产环境资源前缀
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
